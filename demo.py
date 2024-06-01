1160dd2df601adb37a6d2a3b24b673402d0e00be
Maksym Konevych <konevych.m.m@gmail.com>
2024-06-01 20:23:23 +0300

    Add GUI, fix bugs

---
+++ demo.py
@@ -0,0 +1,159 @@
+import tkinter as tk
+import random
+import math
+from typing import Tuple, List
+
+import numpy as np
+
+from main import Polygon, polygon_shortest_path
+
+
+def random_angle_steps(steps: int, irregularity: float) -> List[float]:
+    # generate n angle steps
+    angles = []
+    lower = (2 * math.pi / steps) - irregularity
+    upper = (2 * math.pi / steps) + irregularity
+    cumsum = 0
+    for i in range(steps):
+        angle = random.uniform(lower, upper)
+        angles.append(angle)
+        cumsum += angle
+
+    # normalize the steps so that point 0 and point n+1 are the same
+    cumsum /= (2 * np.pi)
+    for i in range(steps):
+        angles[i] /= cumsum
+    return angles
+
+
+def clip(value, lower, upper):
+    return min(upper, max(value, lower))
+
+
+def generate_polygon(center: Tuple[float, float], avg_radius: float,
+                     irregularity: float, spikiness: float,
+                     num_vertices: int) -> List[Tuple[float, float]]:
+    # Parameter check
+    if irregularity < 0 or irregularity > 1:
+        raise ValueError("Irregularity must be between 0 and 1.")
+    if spikiness < 0 or spikiness > 1:
+        raise ValueError("Spikiness must be between 0 and 1.")
+
+    irregularity *= 2 * math.pi / num_vertices
+    spikiness *= avg_radius
+    angle_steps = random_angle_steps(num_vertices, irregularity)
+
+    # now generate the points
+    points = []
+    angle = random.uniform(0, 2 * math.pi)
+    for i in range(num_vertices):
+        radius = clip(random.gauss(avg_radius, spikiness), 0, 2 * avg_radius)
+        point = (center[0] + radius * math.cos(angle),
+                 center[1] + radius * math.sin(angle))
+        points.append(point)
+        angle += angle_steps[i]
+
+    return points
+
+
+class PolygonApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Shortest Path in Polygon")
+
+        self.canvas = tk.Canvas(root, width=600, height=400, bg="white")
+        self.canvas.pack()
+
+        self.save_polygon_button = tk.Button(root, text="Save Polygon", command=self.save_polygon)
+        self.save_polygon_button.pack()
+
+        self.process_button = tk.Button(root, text="Process", command=self.process)
+        self.process_button.pack()
+
+        self.reset_button = tk.Button(root, text="Reset", command=self.reset)
+        self.reset_button.pack()
+
+        self.save_polygon_button = tk.Button(root, text="Generate Polygon", command=self.generate_polygon)
+        self.save_polygon_button.pack()
+
+        self.vertices = []
+        self.polygon_saved = False
+        self.pt_start = None
+        self.pt_end = None
+        self.canvas.bind("<Button-1>", self.add_point)
+
+    def add_point(self, event):
+        x, y = event.x, event.y
+        if not self.polygon_saved:
+            self.vertices.append((x, y))
+            self.render_vertices()
+        else:
+            if self.pt_start is None:
+                self.pt_start = (x, y)
+                self.canvas.create_oval(x - 3, y - 3, x + 3, y + 3, fill="Blue")
+            elif self.pt_end is None:
+                self.pt_end = (x, y)
+                self.canvas.create_oval(x - 3, y - 3, x + 3, y + 3, fill="Yellow")
+            else:
+                return
+
+    def save_polygon(self):
+        if len(self.vertices) < 3:
+            tk.messagebox.showwarning("Warning", "At least 3 vertices are needed to form a polygon.")
+            return
+
+        self.canvas.create_line(*self.vertices[-1], self.vertices[0], fill="black")
+        self.polygon_saved = True
+
+    def generate_polygon(self):
+        self.vertices = generate_polygon((300, 200), 120, 0.5, 0.4, 70)
+        self.polygon_saved = True
+        self.pt_start = self.pt_end = None
+        self.render_vertices()
+        self.save_polygon()
+
+    def render_vertices(self):
+        self.canvas.delete("all")
+
+        for i in range(len(self.vertices)):
+            x, y = self.vertices[i]
+            self.canvas.create_oval(x - 3, y - 3, x + 3, y + 3, fill="black")
+            self.canvas.create_text(x + 10, y + 10, text=f"{i}", fill='red')
+            if i != 0:
+                self.canvas.create_line(*self.vertices[i-1], x, y, fill="black")
+
+
+    def reset(self):
+        self.vertices = []
+        self.polygon_saved = False
+        self.pt_start = None
+        self.pt_end = None
+        self.canvas.delete('all')
+
+    def process(self):
+        if not self.polygon_saved:
+            tk.messagebox.showwarning("Warning", "Draw and save the polygon")
+            return
+        if self.pt_start is None or self.pt_end is None:
+            tk.messagebox.showwarning("Warning", "Draw start and end points")
+
+        polygon = Polygon(np.asarray(self.vertices, dtype=np.float32))
+        points = polygon_shortest_path(polygon, np.asarray(self.pt_start), np.asarray(self.pt_end))
+
+        def get_point(j):
+            if j == 0:
+                p = self.pt_start
+            elif j == 1:
+                p = self.pt_end
+            else:
+                p = self.vertices[j - 2]
+            return p
+
+        for i1, i2 in zip(points, points[1:]):
+            self.canvas.create_line(*get_point(i1), *get_point(i2), fill="green")
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PolygonApp(root)
+    root.mainloop()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 from typing import NamedTuple, List, Tuple, Generator
 
@@ -7,18 +9,29 @@ class Polygon:
         # todo: points have to be in clockwise order
         assert len(points.shape) == 2 and points.shape[1] == 2, points.shape
         self.points = points
+        self._orient_polygon()
+
+    def _orient_polygon(self, clockwise=True):
+        e = np.asarray(self.edges)
+        rightmost_i = np.argmax(e[:, 1] + e[:, 0], axis=0)[0]
+        edge = self.edges[rightmost_i]
+
+        print((edge[1] - edge[0])[1] < 0)
+        if ((edge[1] - edge[0])[1] < 0) == clockwise:
+            self.points = self.points[::-1]
+
 
     @property
     def edges(self) -> List[Tuple[np.ndarray, np.ndarray]]:
         return [(self.points[i], self.points[(i + 1) % len(self.points)]) for i in range(len(self.points))]
 
 
-def caconical_angle(v):
+def _caconical_angle(v):
     a = np.arctan2(*v)
     return a if a >= 0 else a + 2 * np.pi
 
 
-def get_visible_vertices(point: np.ndarray, polygon: Polygon) -> np.ndarray:
+def _get_visible_vertices(point: np.ndarray, polygon: Polygon) -> np.ndarray:
     # return distance to the point or -1 if it is not visible
 
     point_in_polygon_idx = None  # such points need additional processing
@@ -28,19 +41,19 @@ def get_visible_vertices(point: np.ndarray, polygon: Polygon) -> np.ndarray:
     for i, p in enumerate(polygon.points):
         if np.linalg.norm(p - point) == 0:
             point_in_polygon_idx = i
-        angle = caconical_angle(p - point)
+        angle = _caconical_angle(p - point)
         intersections[angle] = []
 
     if point_in_polygon_idx is not None:
         # restrict possible angles from point
         i_in = point_in_polygon_idx - 1 if point_in_polygon_idx > 0 else len(polygon.edges) - 1
         i_out = point_in_polygon_idx
-        restriction_n1 = polygon.edges[i_out][1] - polygon.edges[i_out][0]
-        restriction_n2 = polygon.edges[i_in][0] - polygon.edges[i_in][1]
+        restriction_n1 = polygon.edges[i_in][0] - polygon.edges[i_in][1]
+        restriction_n2 = polygon.edges[i_out][1] - polygon.edges[i_out][0]
 
     for i, (p_s, p_e) in enumerate(polygon.edges):
-        a_s = caconical_angle(p_s - point)
-        a_e = caconical_angle(p_e - point)
+        a_s = _caconical_angle(p_s - point)
+        a_e = _caconical_angle(p_e - point)
         if a_s > a_e:
             a_s, a_e = a_e, a_s
         if a_e - a_s == np.pi:
@@ -60,11 +73,11 @@ def get_visible_vertices(point: np.ndarray, polygon: Polygon) -> np.ndarray:
             visibilities.append(0)
             continue
         obstructed = False
-        angle = caconical_angle(p - point)
+        angle = _caconical_angle(p - point)
         if point_in_polygon_idx is not None:
-            a_a = caconical_angle(p - point)
-            a_v1 = caconical_angle(restriction_n1)
-            a_v2 = caconical_angle(restriction_n2)
+            a_a = _caconical_angle(p - point)
+            a_v1 = _caconical_angle(restriction_n1)
+            a_v2 = _caconical_angle(restriction_n2)
             if ((a_v1 <= a_v2) and not (a_v1 <= a_a <= a_v2) or
                     (a_v1 > a_v2 and not (a_a <= a_v2 or a_a >= a_v1))):
                 obstructed = True
@@ -91,7 +104,7 @@ def get_visible_vertices(point: np.ndarray, polygon: Polygon) -> np.ndarray:
     return np.asarray(visibilities)
 
 
-def are_directly_visible(point: np.ndarray, target: np.ndarray, polygon: Polygon) -> bool:
+def _are_directly_visible(point: np.ndarray, target: np.ndarray, polygon: Polygon) -> bool:
     for edge_p1, edge_p2 in polygon.edges:
         edge_normal = (edge_p1 - edge_p2) @ np.array([[0, -1], [1, 0]])
         if np.dot(edge_normal, point - edge_p1) * np.dot(edge_normal, target - edge_p1) < 0:
@@ -99,7 +112,7 @@ def are_directly_visible(point: np.ndarray, target: np.ndarray, polygon: Polygon
     return True
 
 
-def construct_visibility_graph(p: np.ndarray, q: np.ndarray, polygon: Polygon) -> np.array:
+def _construct_visibility_graph(p: np.ndarray, q: np.ndarray, polygon: Polygon) -> np.array:
     # construct a connectivity matrix (n+2)x(n+2) where n — number of vertices in polygon
     # M[2:, 2:] describe visibility in polygon
     # The first row/column describes connectivity of p to the rest
@@ -107,62 +120,65 @@ def construct_visibility_graph(p: np.ndarray, q: np.ndarray, polygon: Polygon) -
     # value -1 — not visible, value >=0 — distance between visible points
 
     M = np.zeros((len(polygon.points) + 2, ) * 2)
-    M[0, 1] = M[1, 0] = np.linalg.norm(p - q) if are_directly_visible(p, q, polygon) else -1
+    M[0, 1] = M[1, 0] = np.linalg.norm(p - q) if _are_directly_visible(p, q, polygon) else -1
 
-    M[2:, 0] = M[0, 2:] = get_visible_vertices(p, polygon)
-    M[2:, 1] = M[1, 2:] = get_visible_vertices(q, polygon)
+    M[2:, 0] = M[0, 2:] = _get_visible_vertices(p, polygon)
+    M[2:, 1] = M[1, 2:] = _get_visible_vertices(q, polygon)
 
     # visibilities between polygon vertices
     for i in range(0, len(polygon.points)):
-        M[2 + i, 2:] = M[2:, 2 + i] = get_visible_vertices(polygon.points[i], polygon)
+        M[2 + i, 2:] = M[2:, 2 + i] = _get_visible_vertices(polygon.points[i], polygon)
 
     return M
 
 
-def _min_dist(M: np.ndarray, v: int, memo: np.ndarray, memo_path: List[Tuple[int] | None]) -> Tuple[float, Tuple[int]]:
-    if v == 1:
-        return 0, (1,)
-    if memo[v] >= 0:
-        return float(memo[v]), memo_path[v]
-    if memo[v] == -2:
-        return 1, None
-    min_dist = np.inf
-    min_path = None
-    memo[v] = -2  # currently checking
-    for i, c in enumerate(M[v]):
-        if c >= 1 and i != v and memo[i] != -2:
-            d, p = _min_dist(M, i, memo, memo_path)
-            if p is None:
-                continue
-            d += c
-            p = (v, ) + p
-            memo[i] = d
-            if d < min_dist:
-                min_dist = d
-                min_path = p
-    memo[v] = min_dist
-    memo_path[v] = min_path
-    return min_dist, min_path
+def _run_dijkstra(M: np.ndarray, memo_checked, memo_distance, memo_path, v: int):
+    if memo_checked[v] == 1:
+        return
+    curr_d = memo_distance[v]
+    curr_p = memo_path[v]
+    for i, d in enumerate(M[v]):
+        if d < 0:
+            continue
+        if curr_d + d < memo_distance[i]:
+            memo_distance[i] = curr_d + d
+            memo_path[i] = curr_p + (i,)
+    memo_checked[v] = 1
+    for i in np.argsort(M[v]):
+        if M[v][i] < 0:
+            continue
+        if memo_checked[i] == 1:
+            continue
+        _run_dijkstra(M, memo_checked, memo_distance, memo_path, i)
 
 
-def find_shortest(M: np.ndarray) -> List[int]:
+def _find_shortest(M: np.ndarray) -> List[int]:
     # find the shortest path from first vertex to the second given connectivity matrix
     # Provide costs; if vertices are not connected, set cost negative.
-    memo = np.zeros(M.shape[0]) - 1
+    memo_checked = np.zeros((M.shape[0]))
+    memo_distance = np.zeros((M.shape[0])) + np.inf
     memo_path = [None] * M.shape[0]
-    print(_min_dist(M, 0, memo, memo_path))
-    return memo_path
+    memo_distance[0] = 0
+    memo_path[0] = (0, )
+    _run_dijkstra(M, memo_checked, memo_distance, memo_path, 0)
+    return list(memo_path[1])
+
+
+def polygon_shortest_path(polygon: Polygon, start: np.ndarray, end: np.ndarray) -> List[int]:
+    M = _construct_visibility_graph(start, end, polygon)
+    return _find_shortest(M)
+
 
 def main() -> None:
     polygon = Polygon(np.array([(1, -2), (2, 0), (1, 2), (0, 3), (4, 3), (4, 0), (5, 0), (5, -1)]))
     p = np.array((2, -1))
     q = np.array((1, 2.5))
-    M = construct_visibility_graph(p, q, polygon)
+    M = _construct_visibility_graph(p, q, polygon)
     for r in M:
         for e in r:
             print('{f:.2f}'.format(f=e), end='\t')
         print()
-    print(find_shortest(M))
+    print(_find_shortest(M))
 
 
 if __name__ == '__main__':
